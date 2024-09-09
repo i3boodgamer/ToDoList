add62af547a426715d671aeedbe8f8dbe699bc42
Богдан <91775355+i3boodgamer@users.noreply.github.com>
2024-09-09 16:18:53 +0700

    update field datetime

--- app/core/models/todo_models.py
+++ app/core/models/todo_models.py
@@ -1,9 +1,35 @@
-from base import Base
+from datetime import datetime, timezone
+
+from sqlalchemy import (
+    String,
+    DateTime,
+    func,
+    Boolean,
+    ForeignKey,
+    Integer,
+)
+from sqlalchemy.orm import (
+    Mapped,
+    mapped_column,
+)
+
+from .base import Base
+from .mixins.int_id_pk import IntIDPkMixin
 
 
-class ToDoList(Base):
-    pass
+class ToDoList(IntIDPkMixin, Base):
+    title: Mapped[str] = mapped_column(String(50))
+    create_at: Mapped[datetime] = mapped_column(
+        server_default=func.now(),
+        default=datetime.now(),
+    )
 
 
-class ToDoItem(Base):
-    pass
+class ToDoItem(IntIDPkMixin, Base):
+    title: Mapped[str] = mapped_column(String(50))
+    list_id: Mapped[int] = mapped_column(Integer, ForeignKey(ToDoList.id))
+    completed: Mapped[bool] = mapped_column(Boolean, default=False)
+    create_at: Mapped[datetime] = mapped_column(
+        server_default=func.now(),
+        default=datetime.now(),
+    )
