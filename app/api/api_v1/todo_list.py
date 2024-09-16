f51ca28edfbbb56b82cb1bebeb9046ff0846f346
Богдан <91775355+i3boodgamer@users.noreply.github.com>
2024-09-16 18:19:54 +0700

    add del list; add output of the tasks of the topic; add;

--- app/api/api_v1/todo_list.py
+++ app/api/api_v1/todo_list.py
@@ -2,30 +2,26 @@ from fastapi import (
     APIRouter,
     Depends,
     status
+
 )
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from api.dependencies.todo_item import get_id_item
+from api.dependencies.todo_list import get_id_list
 from crud.todo_list import (
     get_all_todo_list,
     create_todo_list,
     full_todo,
-)
-from crud.todo_item import (
-    get_all_todo_item,
-    create_todo_item,
-    del_todo_item
+    del_todo_list
 )
 from core.models.db_helper import db_helper
-from core.models import ToDoItem
+from core.models.todo_models import ToDoList
 from core.schemas.todo import (
     ToDoListCreate,
-    ToDoItemCreate,
     ToDoListResponse,
 )
 
 
-router = APIRouter()
+router = APIRouter(tags=["Todo List"])
 
 
 @router.get("/list", response_model=list[ToDoListCreate])
@@ -40,26 +36,14 @@ async def create_list_todo(
     return await create_todo_list(todo_item=todo_list, session=session)
 
 
-@router.get("/item", response_model=list[ToDoItemCreate])
-async def get_todo_item(session: AsyncSession = Depends(db_helper.session_getter)):
-    return await get_all_todo_item(session=session)
-
-
-@router.post("/item", response_model=ToDoItemCreate)
-async def create_item_todo(
-    item: ToDoItemCreate, session: AsyncSession = Depends(db_helper.session_getter)
-):
-    return await create_todo_item(todo_item=item, session=session)
-
-
 @router.get("/todo/", response_model=list[ToDoListResponse])
 async def get_todo(session: AsyncSession = Depends(db_helper.session_getter)):
     return await full_todo(session=session)
 
 
-@router.delete("/{item_id}/", status_code=status.HTTP_204_NO_CONTENT)
-async def delete_item_todo(
-    item: ToDoItem = Depends(get_id_item),
-    session: AsyncSession = Depends(db_helper.session_getter),
+@router.delete("/{list_id}/", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_list(
+        todo_list: ToDoList = Depends(get_id_list),
+        session: AsyncSession = Depends(db_helper.session_getter)
 ):
-    return await del_todo_item(todo_item=item, session=session)
+    return await del_todo_list(todo_list=todo_list, session=session)
--- app/crud/todo_item.py
+++ app/crud/todo_item.py
@@ -1,9 +1,11 @@
 from typing import Sequence
 
 from sqlalchemy import Select
+from sqlalchemy.orm import selectinload
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from core.models import ToDoItem
+from core.models import ToDoItem, ToDoList
+from core.schemas.todo import ToDoItemUpdate
 from core.schemas.todo import ToDoItemCreate
 
 
@@ -13,6 +15,27 @@ async def get_all_todo_item(
     stmt = Select(ToDoItem).order_by(ToDoItem.id)
     result = await session.scalars(stmt)
     return result.all()
+
+
+async def get_all_item_list(
+        list_id: ToDoList,
+        session: AsyncSession,
+) -> Sequence[ToDoItem]:
+    stmt = (
+        Select(ToDoItem)
+        .options(selectinload(ToDoItem.todo_list))
+        .where(ToDoItem.list_id == list_id.id)
+        .order_by(ToDoItem.id)
+    )
+    result = await session.scalars(stmt)
+    return result.all()
+
+
+async def get_todo_item(
+        item_id: int,
+        session: AsyncSession,
+) -> ToDoItem | None:
+    return await session.get(ToDoItem, item_id)
 
 
 async def create_todo_item(
@@ -25,9 +48,16 @@ async def create_todo_item(
     return todo_item
 
 
+async def update_todo_item(
+        item: ToDoItem,
+        item_update: ToDoItemUpdate,
+        session: AsyncSession,
+) -> ToDoItem:
+    for title, completed in item_update.model_dump(exclude_unset=True).items():
+        setattr(item, title, completed)
 
-async def update_todo_item():
-    pass
+    await session.commit()
+    return item
 
 
 async def del_todo_item(
