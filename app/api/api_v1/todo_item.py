73d349eff88281698f0e9d0ed45873896ab2bccd
Богдан <91775355+i3boodgamer@users.noreply.github.com>
2024-09-21 17:19:00 +0700

    add a list only to authorized users; add relationship table ToDoList and User

--- app/api/api_v1/todo_item.py
+++ app/api/api_v1/todo_item.py
@@ -5,19 +5,21 @@ from fastapi import (
 )
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from api.dependencies.authentication.fastapi_user_router import current_user
 from api.dependencies.todo_item import get_id_item
+from api.dependencies.todo_list import get_id_list
 from crud.todo_item import (
     get_all_todo_item,
     create_todo_item,
     del_todo_item,
-    update_todo_item
+    update_todo_item,
+    get_all_item_list
 )
 from core.models.db_helper import db_helper
-from core.models import ToDoItem
+from core.models import ToDoItem, ToDoList, User
 from core.schemas.todo import (
     ToDoItemCreate,
     ToDoItemUpdate,
-    TodoItemUpdate,
 )
 
 
@@ -29,16 +31,22 @@ async def get_todo_item(session: AsyncSession = Depends(db_helper.session_getter
     return await get_all_todo_item(session=session)
 
 
+@router.get("/{list_id}/", response_model=list[ToDoItemUpdate])
+async def get_all_list_item(list_at: ToDoList = Depends(get_id_list), session: AsyncSession = Depends(db_helper.session_getter)):
+    return await get_all_item_list(list_id=list_at, session=session)
+
+
 @router.post("/item", response_model=ToDoItemCreate)
 async def create_item_todo(
-    item: ToDoItemCreate, session: AsyncSession = Depends(db_helper.session_getter)
+        item: ToDoItemCreate,
+        session: AsyncSession = Depends(db_helper.session_getter)
 ):
     return await create_todo_item(todo_item=item, session=session)
 
 
 @router.patch("/{item_id}/", response_model=ToDoItemUpdate)
 async def update_item(
-        todo_item_update: TodoItemUpdate,
+        todo_item_update: ToDoItemUpdate,
         item: ToDoItem = Depends(get_id_item),
         session: AsyncSession = Depends(db_helper.session_getter),
 ):
